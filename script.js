ee8d831d4ebbae9d6c37d11f7ebb35305e77e14e
agizzi <alexandra.n.gizzi@gmail.com>
2019-09-23 18:05:45 -0400

    finish

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const capitalize = (string) => v.titleCase(string);
 // test function with string below
 console.log(capitalize('i like to poop'));
+
+function customerToHTML(customer) {
+	return `<div class="customerBox">
+            <img src="${customer.picture.large}" class="largePhoto">
+            <h2 class="customerName">${capitalize(customer.name.first)} ${capitalize(customer.name.last)}</h2>
+            <ul class="customerInfo">
+                <li class ="customerEmail">${customer.email}</li>
+                <li class="customerAddressLine1">${capitalize(customer.location.street)}</li>
+                <li class="customerAddressLine2">${capitalize(customer.location.city)}, ${nameToAbbr(
+		capitalize(customer.location.state)
+	)} ${capitalize(customer.location.postcode)}</li>
+                <li class="customerDOB"> DOB: ${moment(customer.dob).format('MMMM Do YYYY')}</li>
+                <li class="customerSince">Customer Since: ${moment(customer.registered).format('MMMM Do YYYY')}</li>
+            </ul>
+        </div>`;
+}
+
+document.querySelector('.container').innerHTML = customers.map(customerToHTML).join('\n');
